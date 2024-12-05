d0ce5e41d85167f398cf21d9c0b950bd72143c0c
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2024-12-06 00:37:32 +0545

    did some minor adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,21 +13,7 @@ const Home = () => {
     });
   }, []);
   if (user) {
-    if (posts.length === 0) {
-      return (
-        <div className="w-full py-8 mt-4 text-center">
-          <Container>
-            <div className="flex flex-wrap">
-              <div className="p-2 w-full">
-                <h1 className="text-2xl font-bold hover:text-gray-500">
-                  No posts yet!
-                </h1>
-              </div>
-            </div>
-          </Container>
-        </div>
-      );
-    } else {
+    if (posts.length > 0) {
       return (
         <div className="w-full py-8">
           <Container>
@@ -37,6 +23,20 @@ const Home = () => {
                   <PostCard {...post} />
                 </div>
               ))}
+            </div>
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div className="w-full py-8 mt-4 text-center">
+          <Container>
+            <div className="flex flex-wrap">
+              <div className="p-2 w-full">
+                <h1 className="text-2xl font-bold hover:text-gray-500">
+                  No posts yet!
+                </h1>
+              </div>
             </div>
           </Container>
         </div>
