b50b2b4bcffdb01c45493b4528c94bbe123a7f5e
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2024-12-06 00:20:50 +0545

    do some minor fixes

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -4,7 +4,6 @@ import appwriteService from "../appwrite/config";
 import { useSelector } from "react-redux";
 const AllPosts = () => {
   const [posts, setPosts] = useState([]);
-  // const [userData, setUserData] = useState({});
   const userData = useSelector((state) => state.auth.userData);
   useEffect(() => {
     appwriteService.getAllPosts("", userData.$id).then((res) => {
@@ -13,22 +12,35 @@ const AllPosts = () => {
       }
     });
   }, []);
-  console.log(posts);
-  console.log(userData);
-
-  return (
-    <div className="w-full py-8">
-      <Container>
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="py-4">
-              <PostCard {...post} />
+  if (posts.length > 0) {
+    return (
+      <div className="w-full py-8">
+        <Container>
+          <div className="flex flex-wrap">
+            {posts?.map((post) => (
+              <div key={post.$id} className="py-4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                No posts created yet!
+              </h1>
             </div>
-          ))}
-        </div>
-      </Container>
-    </div>
-  );
+          </div>
+        </Container>
+      </div>
+    );
+  }
 };
 
 export default AllPosts;
