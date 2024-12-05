3d50495b1ab0580104819f75c92ec3db777ce555
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2024-12-06 00:35:55 +0545

    some minor adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,37 +12,39 @@ const Home = () => {
       }
     });
   }, []);
-
-  if (posts.length === 0) {
-    return (
-      <div className="w-full py-8 mt-4 text-center">
-        <Container>
-          <div className="flex flex-wrap">
-            <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-gray-500">
-                {user
-                  ? "No posts yet!"
-                  : "Please register or login to read posts"}
-              </h1>
+  if (user) {
+    if (posts.length === 0) {
+      return (
+        <div className="w-full py-8 mt-4 text-center">
+          <Container>
+            <div className="flex flex-wrap">
+              <div className="p-2 w-full">
+                <h1 className="text-2xl font-bold hover:text-gray-500">
+                  No posts yet!
+                </h1>
+              </div>
             </div>
-          </div>
-        </Container>
-      </div>
-    );
-  }
-  return (
-    <div className="w-full py-8">
-      <Container>
-        <div className="flex flex-wrap gap-5">
-          {posts.documents.map((post) => (
-            <div key={post.$id} className="py-4">
-              <PostCard {...post} />
-            </div>
-          ))}
+          </Container>
         </div>
-      </Container>
-    </div>
-  );
+      );
+    } else {
+      return (
+        <div className="w-full py-8">
+          <Container>
+            <div className="flex flex-wrap gap-5">
+              {posts.documents.map((post) => (
+                <div key={post.$id} className="py-4">
+                  <PostCard {...post} />
+                </div>
+              ))}
+            </div>
+          </Container>
+        </div>
+      );
+    }
+  } else {
+    return <div>Please register or login to read posts</div>;
+  }
 };
 
 export default Home;
