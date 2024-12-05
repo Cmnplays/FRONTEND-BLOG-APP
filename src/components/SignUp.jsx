27463c54dcb5d16f5b0051e9e2298bfedd083e23
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2024-12-05 23:13:50 +0545

    resolving errors of signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -11,9 +11,10 @@ const SignUp = () => {
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
   const create = async (data) => {
-    setError("");
     try {
+      console.log(data);
       const userData = await authService.createAccount(data);
+      console.log(userData);
       if (userData) {
         dispatch(login(userData));
         navigate("/");
@@ -60,11 +61,11 @@ const SignUp = () => {
             type="email"
             {...register("email", {
               required: true,
-              // validate: {
-              //   matchPattern: (value) =>
-              //     /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-              //     "Email address must be a valid adddress",
-              // },
+              validate: {
+                matchPattern: (value) =>
+                  /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                  "Email address must be a valid adddress",
+              },
             })}
           />
 
